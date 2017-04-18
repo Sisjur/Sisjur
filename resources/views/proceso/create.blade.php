4eb253801bf0c1182d0c65546d7944fc99f1f7d4
Eliam <elvaleli@gmail.com>
2017-04-17 22:41:47 -0500

    Cambios vista

--- resources/views/proceso/create.blade.php
+++ resources/views/proceso/create.blade.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
 /**
  * Created by PhpStorm.
  * User: Eliam
@@ -17,7 +17,7 @@
                 <div class="col-md-offset-3 col-md-5 col-sm-4" id="msj">
                     @if (isset($msj))
                         <div  class="alert alert-success alert-dismissible"  role="alert" style="margin-bottom : -5px;margin-top : -5px;z-index:2;">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>                    
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                             {{$msj}}
                         </div>
                     @endif
@@ -114,7 +114,7 @@
         animation_title("Registrar Proceso");
 
 
-      
+
         //mascara para celular
         $("input[name=txt_celular]").inputmask("mask", {"mask": "(999) 999-9999"});
         //solo admitir letras
