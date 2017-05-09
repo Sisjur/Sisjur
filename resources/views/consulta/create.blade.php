5f4018ba73416cf362b61d361484f63776651aa5
Eliam <elvaleli@gmail.com>
2017-05-09 07:15:38 -0500

    Cambios consultas

--- resources/views/consulta/create.blade.php
+++ resources/views/consulta/create.blade.php
@@ -15,12 +15,16 @@
                 </div>
 
                 <div class="col-md-offset-3 col-md-5 col-sm-4" id="msj">
-                    @if (isset($msj))
-                        <div  class="alert alert-success alert-dismissible"  role="alert" style="margin-bottom : -5px;margin-top : -5px;z-index:2;">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            {{$msj}}
-                        </div>
-                    @endif
+                  @if (count($errors) > 0)
+                  <div class="alert alert-success alert-dismissible" role="alert" style="margin-bottom : -5px;margin-top : -5px;z-index:2;">
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      <ul>
+                          @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                          @endforeach
+                      </ul>
+                  </div>
+                  @endif
                 </div>
             </div>
         </section>
